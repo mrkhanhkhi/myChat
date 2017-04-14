3361b407107066b59e5b937ba5d1c3284a424dad
Nguyen Duy Khanh <dLn-wN7-GMe-d9pkhanhnd@runsystem.net>
2017-04-14 17:59:26 +0700

    first commit

---
+++ MyChat/LoginController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginController.swift
+//  MyChat
+//
+//  Created by Nguyen Duy Khanh on 4/13/17.
+//  Copyright © 2017 Nguyen Duy Khanh. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
